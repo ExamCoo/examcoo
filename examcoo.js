4784ebe07876ef69c3797323e368147daa030b20
shangdawei <shangdawei@gmail.com>
2020-06-03 13:34:45 +0800

    a

--- examcoo.js
+++ examcoo.js
@@ -1,6 +1,28 @@
-  alert("ExamCoo.js 加载完成!");
+jQueryTemp = jQuery.noConflict(true);
+jQueryOriginal = jQuery || jQueryTemp;
 
-  jQ(function() {
-    alert("在页面的dom元素加载完毕后就执行，而无需等到图片或其他媒体下载完毕。");
-  });
+head = document.getElementsByTagName("head")[0];
+script = document.createElement("script");
+script.id = scriptId;
+script.type = "text/javascript";
+script.src = "https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js";
+head.appendChild(script);
+
+script.onload = function() {
+  jQ = jQuery.noConflict(true);
+  window.jQuery = window.$ = jQueryOriginal;
+
+  script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "https://cdn.bootcdn.net/ajax/libs/marked/1.1.0/marked.min.js";
+  head.appendChild(script);
+
+  linkid = "examcoocss";
+  link = document.createElement("link");
+  link.id = linkid;
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = "https://cdn.jsdelivr.net/gh/examcoo/examcoo/examcoo.css";
+  head.appendChild(link);
+}
 

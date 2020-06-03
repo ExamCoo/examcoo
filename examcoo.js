d594d61f1c38b2f7752e8e37878b6bc67578620a
shangdawei <shangdawei@gmail.com>
2020-06-03 13:47:04 +0800

    a

--- examcoo.js
+++ examcoo.js
@@ -1,22 +1,18 @@
 jQueryTemp = jQuery.noConflict(true);
 jQueryOriginal = jQuery || jQueryTemp;
-
 head = document.getElementsByTagName("head")[0];
 script = document.createElement("script");
 script.id = scriptId;
 script.type = "text/javascript";
 script.src = "https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js";
 head.appendChild(script);
-
 script.onload = function() {
   jQ = jQuery.noConflict(true);
   window.jQuery = window.$ = jQueryOriginal;
-
   script = document.createElement("script");
   script.type = "text/javascript";
   script.src = "https://cdn.bootcdn.net/ajax/libs/marked/1.1.0/marked.min.js";
   head.appendChild(script);
-
   linkid = "examcoocss";
   link = document.createElement("link");
   link.id = linkid;
@@ -25,4 +21,7 @@ script.onload = function() {
   link.href = "https://cdn.jsdelivr.net/gh/examcoo/examcoo/examcoo.css";
   head.appendChild(link);
 }
-
+$.ajaxComplete(function(e, xhr, settings) {
+  alert('it worked, and the response was:' + xhr.responseHTML);
+  alert('and the original URL was:' + settings.url);
+});
